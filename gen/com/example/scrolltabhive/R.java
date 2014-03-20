ae9c71198c7a643351f2a202601d34098e157715
Max Rogers <maxrogers90@gmail.com>
2014-03-20 14:34:13 -0400

    second commit

--- gen/com/example/scrolltabhive/R.java
+++ gen/com/example/scrolltabhive/R.java
@@ -22,13 +22,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
+        public static final int action_settings=0x7f09000c;
+        public static final int btnChangeDate=0x7f090002;
+        public static final int btnChangeTime=0x7f090007;
+        public static final int btnCountDown=0x7f090005;
+        public static final int lblDate=0x7f090003;
+        public static final int lblTime=0x7f090008;
         public static final int main_layout=0x7f090000;
         public static final int pager=0x7f090001;
-        public static final int textView1=0x7f090003;
-        public static final int videoView1=0x7f090002;
+        public static final int textView1=0x7f09000b;
+        public static final int tvDate=0x7f090004;
+        public static final int tvTime=0x7f090009;
+        public static final int tvTimeLeft=0x7f090006;
+        public static final int videoView1=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
